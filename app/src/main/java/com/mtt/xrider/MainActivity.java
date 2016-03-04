7add8462d8f7d8ca2ea7e02d94e9814779cb3cb4
Kerry Zhang <zhangke@mttsmart.com>
2016-03-04 09:42:59 +0800

    update: base UI frame

--- app/src/main/java/com/mtt/xrider/MainActivity.java
+++ app/src/main/java/com/mtt/xrider/MainActivity.java
@@ -1,52 +1,84 @@
 package com.mtt.xrider;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends XRiderBaseActivity implements View.OnClickListener,
+        NavigationView.OnNavigationItemSelectedListener{
+
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    private CollapsingToolbarLayout collapsingToolbarLayout;
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private FloatingActionButton floatingActionButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        initViews();
+        initDrawer();
+    }
+
+    /**
+     * 初始化基础View
+     * */
+    private void initViews() {
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // 标题
+        collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.main_collapse_layout);
+        collapsingToolbarLayout.setTitle(" ");
+
+        setStatusBarColorForKitkat(R.color.color_status_bar);
+
+        //fab
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.main_fab);
+        floatingActionButton.setOnClickListener(this);
+    }
+
+    /**
+     * 初始化抽屉
+     */
+    private void initDrawer() {
+        NavigationView navigationView = (NavigationView) findViewById(R.id.main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View headerLayout = navigationView.inflateHeaderView(R.layout.nav_header_main);
+        drawerLayout = (DrawerLayout) findViewById(R.id.main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public boolean onNavigationItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_set:
+                break;
+            case R.id.nav_about:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 }
